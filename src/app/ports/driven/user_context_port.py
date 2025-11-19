6441f0fe721a05c3dc8c611e85a46121c875b54e
Navid <navid.mhkh@gmail.com>
2025-11-19 15:14:40 +0300

    feat(user_context_port): initial user_context_port commit.

---
+++ src/app/ports/driven/user_context_port.py
@@ -0,0 +1,14 @@
+from abc import ABC, abstractmethod
+from src.app.auth.users import User
+
+
+class UserContextPort(ABC):
+
+    @abstractmethod
+    def current_user(self) -> User | None:
+        """
+        Retrieves the current user. Each adapter should implement this method based on the logics of the connection type
+        it is using.
+        :return: A User object
+        """
+        pass
