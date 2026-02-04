3460fdf9768e8ad28f946221396ffedf109fcb0f
Navid <navid.mhkh@gmail.com>
2026-02-04 07:08:00 +0300

    feat(DI): a simple composition root developed

---
+++ src/app/container.py
@@ -0,0 +1,10 @@
+from src.app.user_management.application.services.user_application_service import UserApplicationService
+from src.app.user_management.application.use_cases.create_user_use_case import CreateUserUseCase
+from tests.user_management.adapters.driven.fake_user_repo import FakeUserRepoInMemory
+
+
+class Container:
+    def __init__(self):
+        self.user_repo = FakeUserRepoInMemory()
+        self.create_user_uc = CreateUserUseCase(self.user_repo)
+        self.user_application_service = UserApplicationService(self.create_user_uc)
