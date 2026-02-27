41a4a86c7af7c6ba380c4f4df2f0e01ef625b181
Navid <navid.mhkh@gmail.com>
2026-02-27 22:23:26 +0300

    feat(startup): FastAPI lifespan added to initialize and close the DB engine

--- src/app/main.py
+++ src/app/main.py
@@ -1,3 +1,4 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from src.app.container import Container
 from src.app.infrastructure.persistence.db.session import init_engine, close_engine
@@ -6,20 +7,17 @@ from src.app.user_management.adapters.driving.fast_api.controllers.user_controll
 )
 from src.app.interfaces.http.fast_api.handlers import register_api_exception_handler
 
-container = Container()
-app = FastAPI()
-app.container = container
 
-
-@app.on_event("startup")
-async def on_startup():
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # start up
     init_engine()
-
-
-@app.on_event("shutdown")
-async def on_shutdown():
+    app.container = Container()
+    yield
+    # shutdown
     await close_engine()
 
 
+app = FastAPI(lifespan=lifespan)
 app.include_router(user_v1)
 register_api_exception_handler(app)
