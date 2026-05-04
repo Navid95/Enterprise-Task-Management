1bc61555b13c69b65a73447ced6e844ea3ec27e8
Navid <navid.mhkh@gmail.com>
2026-05-04 19:15:41 +0300

    fix: return 401 with WWW-Authenticate header for missing or invalid Authorization

--- src/app/user_management/adapters/driving/fast_api/dependencies/auth_dependencies.py
+++ src/app/user_management/adapters/driving/fast_api/dependencies/auth_dependencies.py
@@ -1,9 +1,39 @@
+from typing import Optional
 from uuid import UUID
 
-from fastapi import Depends, Request
+from fastapi import Depends, Request, HTTPException
 from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi.security.utils import get_authorization_scheme_param
+from starlette.status import HTTP_401_UNAUTHORIZED
 
-bearer = HTTPBearer()
+
+class JWTBearer(HTTPBearer):
+    async def __call__(
+            self, request: Request
+    ) -> Optional[HTTPAuthorizationCredentials]:
+        authorization = request.headers.get("Authorization")
+        scheme, credentials = get_authorization_scheme_param(authorization)
+        if not (authorization and scheme and credentials):
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED, detail="Not authenticated",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        if scheme.lower() != "bearer":
+            if self.auto_error:
+                raise HTTPException(
+                    status_code=HTTP_401_UNAUTHORIZED,
+                    detail="Invalid authentication credentials",
+                    headers={"WWW-Authenticate": "Bearer"},
+                )
+            else:
+                return None
+        return HTTPAuthorizationCredentials(scheme=scheme, credentials=credentials)
+
+
+bearer = JWTBearer()
 
 
 def get_authentication_service(request: Request):
@@ -11,6 +41,6 @@ def get_authentication_service(request: Request):
 
 
 def current_user(
-    request: Request, credentials: HTTPAuthorizationCredentials = Depends(bearer)
+        request: Request, credentials: HTTPAuthorizationCredentials = Depends(bearer)
 ) -> UUID:
     return request.app.container.get_token_service().verify_token(credentials.credentials)
